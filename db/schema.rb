0a5a67ff1dd24de6800e95171875eb89fbd87644
Rodolfo Ruiz <rpruiz@me.com>
2020-06-18 11:09:13 -0500

    Postgres preparation for UUID

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_141607) do
+ActiveRecord::Schema.define(version: 2020_06_18_160718) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
